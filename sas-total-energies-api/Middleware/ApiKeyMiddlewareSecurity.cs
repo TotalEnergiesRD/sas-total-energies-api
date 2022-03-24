26582cb242f050fbc01ca4109346b09bf22a0267
jannabelramos <jannabelramos@gmail.com>
2022-03-23 20:49:19 -0400

    Project Settings

---
+++ sas-total-energies-api/Middleware/ApiKeyMiddlewareSecurity.cs
@@ -0,0 +1,35 @@
+﻿namespace sas_total_energies_api.Middleware
+{
+    public class ApiKeyMiddlewareSecurity
+    {
+
+        private readonly RequestDelegate _next;
+        private const string APIKEY = "TotalApiKey";
+        public ApiKeyMiddlewareSecurity(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue(APIKEY, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("No se ha proporcionado un ApiKey");
+                return;
+            }
+
+            var appSettings = context.RequestServices.GetRequiredService<IConfiguration>();
+
+            var apiKey = appSettings.GetValue<string>(APIKEY);
+
+            if (!apiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Cliente no autorizado");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
